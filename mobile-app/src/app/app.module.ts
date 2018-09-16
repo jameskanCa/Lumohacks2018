c358f530253a1532045048e735156d5f302b8a9e
anlinpanda <anlin_c@yahoo.ca>
2018-09-16 08:07:03 -0700

    login page working

--- mobile-app/src/app/app.module.ts
+++ mobile-app/src/app/app.module.ts
@@ -4,23 +4,27 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Camera } from '@ionic-native/camera';
-
+import { IonicSwipeAllModule } from 'ionic-swipe-all';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LogIn } from '../pages/log-in/log-in';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LogIn
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicSwipeAllModule 
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LogIn
   ],
   providers: [
     StatusBar,
