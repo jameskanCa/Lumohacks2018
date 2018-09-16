5329241eddfe379c62891bc197691c09f779c427
Hai Lin Zhang <hailin.zhang@fatiguescience.com>
2018-09-16 04:47:45 -0700

    image push working

---
+++ mobile-app/src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB_PX7KEbos9jqbkExry4H6q81EaCEV4wM",
+  authDomain: "shift-7ceab.firebaseapp.com",
+  databaseURL: "https://shift-7ceab.firebaseio.com",
+  projectId: "shift-7ceab",
+  storageBucket: "shift-7ceab.appspot.com",
+  messagingSenderId: "1032454189095"
+}
